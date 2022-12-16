90e1ed9d1fb8dd943227844ee3e4ca1e13842c5c
DevExpressExampleBot <fakeEmail>
2022-12-17 03:33:56 +0400

    Source auto update [skip ci]

--- VB/ViewModel/ViewModel.vb
+++ VB/ViewModel/ViewModel.vb
@@ -1,33 +1,29 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace WpfApplication41
-	Public Class ViewModel
-		Public Overridable Property FixedTopRows() As ObservableCollection(Of Customer)
-		Public Property Customers() As ObservableCollection(Of Customer)
 
-		Public Sub New()
-			Customers = New ObservableCollection(Of Customer)()
-			For i As Integer = 1 To 29
-				Customers.Add(New Customer() With {
-					.ID = i,
-					.Name = "Name" & i
-				})
-			Next i
+    Public Class ViewModel
 
-			FixedTopRows = New ObservableCollection(Of Customer)()
-			FixedTopRows.Add(Customers(5))
-			FixedTopRows.Add(Customers(20))
-		End Sub
-	End Class
+        Public Overridable Property FixedTopRows As ObservableCollection(Of Customer)
 
-	Public Class Customer
-		Public Property ID() As Integer
+        Public Property Customers As ObservableCollection(Of Customer)
 
-		Public Property Name() As String
-	End Class
+        Public Sub New()
+            Customers = New ObservableCollection(Of Customer)()
+            For i As Integer = 1 To 30 - 1
+                Customers.Add(New Customer() With {.ID = i, .Name = "Name" & i})
+            Next
+
+            FixedTopRows = New ObservableCollection(Of Customer)()
+            FixedTopRows.Add(Customers(5))
+            FixedTopRows.Add(Customers(20))
+        End Sub
+    End Class
+
+    Public Class Customer
+
+        Public Property ID As Integer
+
+        Public Property Name As String
+    End Class
 End Namespace
